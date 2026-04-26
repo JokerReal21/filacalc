f6a0f429b0c12de14757610550309a27602594ca
Edwin Porras <edwin.porras1022@gmail.com>
2026-04-25 22:43:16 -0600

    Refactor ad script inclusion in layout to use standard <script> tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter, Instrument_Serif, JetBrains_Mono } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -40,13 +39,14 @@ export default function RootLayout({
       lang="es"
       className={`${inter.variable} ${instrumentSerif.variable} ${jetbrainsMono.variable}`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8647038916640992"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8647038916640992"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
     </html>
   );
 }
